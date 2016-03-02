f0e85c5680579fd142fb40674745fccb92086e6b
Joshwin Greene <joshwin.greene@gmail.com>
2016-03-02 01:22:53 -0800

    - Added a 2D array that contains references to the nodes that are placed on the GridPane
    	- Will be used to allow each game to identify and alter the game's pieces

--- src/GeneralGameBoard/GridPaneTesting.java
+++ src/GeneralGameBoard/GridPaneTesting.java
@@ -21,6 +21,8 @@ public class GridPaneTesting extends Application implements EventHandler<ActionE
     GridPane myGridPane;
     //Button button;
 
+    IndexButton[][] buttonArray; // Used to keep a record of the pieces placed on the gridpane and allow game events to alter the board's pieces
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
@@ -47,12 +49,15 @@ public class GridPaneTesting extends Application implements EventHandler<ActionE
 
         layout.setStyle("-fx-padding: 20px;");
 
-        // i - column (x), j - row (y)
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 5; j++) {
-                IndexButton temp = new IndexButton("Hello", i, j); // TODO: Do something similar with the imageview class that I will be creating
-                temp.setOnAction(this);
-                layout.add(temp, i, j);
+        buttonArray = new IndexButton[4][5];
+
+        // c - column (x), r - row (y)
+        for (int c = 0; c < 4; c++) {
+            for (int r = 0; r < 5; r++) {
+                IndexButton temp = new IndexButton("Hello", c, r); // TODO: Do something similar with the imageview class that I will be creating
+                temp.setOnAction(this); // TODO: Events could be dealt with in some other location
+                layout.add(temp, c, r);
+                buttonArray[c][r] = temp; // Keep a record of the node
             }
         }
 
@@ -70,7 +75,10 @@ public class GridPaneTesting extends Application implements EventHandler<ActionE
     public void handle(ActionEvent event) {
         //System.out.println("Button Clicked");
         System.out.println(((IndexButton) event.getSource()).getRowColIndex());
+
+        buttonArray[2][2].setText("What?");
     }
+
 
     public class IndexButton extends Button {
 
