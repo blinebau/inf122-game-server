c131e543657997594bbe2fcafd56db5627b3e19c
Jiahao Chen <cjhjames89@gmail.com>
2016-03-03 17:09:02 -0800

    Updated Move class. Make it serializable so that it can transfer via socket.

--- src/models/Move.java
+++ src/models/Move.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.awt.Point;	// favor awt Point over javafx Point2D because awt.Point is recorded with int coordinates
+import java.io.Serializable;
 
 /**
  * 
@@ -8,11 +9,11 @@ import java.awt.Point;	// favor awt Point over javafx Point2D because awt.Point 
  *
  */
 
-public class Move {
+public class Move implements Serializable {
 
-	private Point source;
-	private Point destination;
-	private boolean isMyTurn;	// Change the variable name if it is not descriptive enough.
+	private final Point source;
+	private final Point destination;
+	private final boolean isMyTurn;	// Change the variable name if it is not descriptive enough.
 	/*
 	 * 	boolean variable is used to indicate whether one player is done with his turn.
 	 * 	In Checkers, a player can jump multiple times when there are multiple opponent's pieces can be captured.
