61f6f4e313d02b5fc45bb7d154777e146701448d
Jiahao Chen <cjhjames89@gmail.com>
2016-03-05 15:35:40 -0800

    Skeleton of controller super class BoardGameController

--- src/app/controller/BoardGameController.java
+++ src/app/controller/BoardGameController.java
@@ -1,7 +1,30 @@
 package app.controller;
 
+import java.util.List;
+import GeneralGameBoard.BoardIndex;
+import app.model.GameState;
+import app.view.GameGUI;
+import BoardServer.BoardClient;
+
 /**
  * Created by Roy on 3/5/16.
  */
-public class BoardGameController {
+public abstract class BoardGameController {
+	
+	private GameState state;
+	private GameGUI gui;
+	private List<BoardIndex> validMoves;
+	private final BoardClient client;
+	
+	public BoardGameController(BoardClient c) {
+		client = c;
+	}
+	
+	protected abstract void makeMove(BoardIndex pos);
+	
+	protected abstract boolean validateMove(BoardIndex pos);
+	
+	protected abstract void updateModel();
+	
+	protected abstract void updateView();
 }
