40ddd17f1f221719ec0c59fdd03a3771aa411c01
Jiahao Chen <cjhjames89@gmail.com>
2016-03-10 20:31:23 -0800

    Deleted isMyTurn from Move class.

--- src/app/model/Move.java
+++ src/app/model/Move.java
@@ -1,8 +1,6 @@
 package app.model;
 
-//import Chess.Logic.Board;
 import app.model.BoardIndex;
-
 import java.io.Serializable;
 
 
@@ -12,31 +10,15 @@ import java.io.Serializable;
  *
  */
 
-public class Move implements Serializable{
+public class Move implements Serializable {
 
-    private BoardIndex index;
-	//tuple for source and dest; represented as pair or multiple values
-	//private final int  source;
-	//private final int destination;
-	private final boolean isMyTurn = false;	// Change the variable name if it is not descriptive enough.
+    private final BoardIndex dest;
 
-	/*
-	 * 	boolean variable is used to indicate whether one player is done with his turn.
-	 * 	In Checkers, a player can jump multiple times when there are multiple opponent's pieces can be captured.
-	 *  But the move is sent with one src and one dest
-	 *  The receiver of a Move object need to check if he can make his move or he needs to wait for another move from the opponent.
-	 */
-
-    public Move(BoardIndex index)
-    {
-        this.index = index;
+    public Move(BoardIndex index) {
+        this.dest = index;
     }
 
-	public BoardIndex getIndex() {
-		return index;
-	}
-
-	public boolean isMyTurn() {
-		return isMyTurn;
+	public BoardIndex getdDest() {
+		return dest;
 	}
 }
