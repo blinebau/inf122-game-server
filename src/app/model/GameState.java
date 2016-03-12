953d708549c746248dbbbf93b67c6dbf82d00657
Sophia Chan <sophiawc@uci.edu>
2016-03-12 12:16:36 -0800

    got rid of repeat methods in class GameState

--- src/app/model/GameState.java
+++ src/app/model/GameState.java
@@ -12,23 +12,11 @@ public class GameState {
 		board[index.getColumnIndex()][index.getRowIndex()] = piece;
 	}
 	
-	public void putPiece(Piece piece , int col, int row) {
-		board[col][row] = piece;
-	}
-	
 	public Piece getIndex(BoardIndex index) {
 		return board[index.getColumnIndex()][index.getRowIndex()];
 	}
 	
-	public Piece getIndex(int col, int row) {
-		return board[col][row];
-	}
-	
 	public void removePiece(BoardIndex index) {
 		board[index.getColumnIndex()][index.getRowIndex()] = null;
-	}
-	
-	public void removePiece(int col, int row) {
-		board[col][row] = null;
 	}
 }
