9e3da429095f874e7582e7788349acc7a524e321
Sophia Chan <sophiawc@uci.edu>
2016-03-12 12:38:32 -0800

    started working on modifying TicTacToe to updated design

--- src/TicTacToe/TTTController.java
+++ src/TicTacToe/TTTController.java
@@ -3,13 +3,24 @@ package TicTacToe;
 import BoardServer.BoardClient;
 import app.controller.BoardGameController;
 import app.model.BoardIndex;
+import app.model.GameState;
 import app.model.Move;
+import app.model.Piece;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
  * Created by Sophia on 3/12/2016.
  */
 public class TTTController extends BoardGameController {
+
+    private final Image mortalKombatX = new Image("MortalKombatX");
+    private final Image mortalKombatO = new Image("MortalKombatO");
+
+    private Piece X = new TTTPiece(mortalKombatX);
+    private Piece O = new TTTPiece(mortalKombatO);
+
+    private GameState gameState = new GameState(3, 3);
 
     public TTGUI getGameGUI() {
         return gameGUI;
